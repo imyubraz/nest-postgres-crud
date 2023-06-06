60c65bee32b67d9a8a2c4f24c5a597b25fd9cfdd
Yubraj <iyubrajpoudel@gmail.com>
2023-06-06 12:07:33 +0545

    Defining user creation DTO...

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    firstName: string;
+    lastname: string;
+    age: number
+}
+
+// DTO : Data Transfer object
+// Defining DTO (defining structure of data we expect from the user/client)
