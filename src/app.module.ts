be4aad4b0754a0814daaed18f2db6532bf85df06
Yubraj <iyubrajpoudel@gmail.com>
2023-06-06 14:09:57 +0545

    making .env default environment variables file to ease deployment...

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { UserModule } from './user/user.module';
   imports: [ConfigModule.forRoot({
     isGlobal: true,
       // by making it true we can use this config module throughout the application (for every further modules created). If its false we must import config module in every modules manually (like here we did)
-    envFilePath: ".local.env"
+    // envFilePath: ".local.env"
     // envFilePath: ".prod.env"
       // if not mentioned, then default path for env file would be ".env"
 
